c367c3f120c9e8df11e9acc8477cea126930cac1
lxh6688 <1272662790@qq.comom>
2021-02-22 22:54:32 +0800

    深拷贝  4种方式实现

--- src/function/13.clone.js
+++ src/function/13.clone.js
@@ -26,3 +26,72 @@ function clone2(target){
     return target
   }
 }
+
+function deepClone1(target){
+  let str = JSON.stringify(target)
+  let data = JSON.parse(str)
+  return data
+}
+
+function deepClone2(target){
+ if(typeof target === 'object' && target !== null){
+   const result = Array.isArray(target) ? [] : {}
+   for(let key in target){
+    //  检测该属性是否为对象本身的属性(不能拷贝原型对象的属性)
+     if(target.hasOwnProperty(key)){
+       result[key] = deepClone2(target[key])
+     }
+   }
+   return result
+ }else {
+    return target
+ }
+}
+
+function deepClone3(target,map=new Map()){
+ if(typeof target === 'object' && target !== null){
+    // 克隆数据之前，进行判断，数据之前是否克隆过  
+    let cache = map.get(target)
+    if(cache){
+     return cache
+    }
+    const result = Array.isArray(target) ? [] : {}
+    // 将新的结果存入到容器中
+    map.set(target, result)
+   for(let key in target ){
+    //  检测该属性是否为对象本身的属性(不能拷贝原型对象的属性)
+     if(target.hasOwnProperty(key)){
+       result[key] = deepClone3(target[key],map)
+     }
+   }
+   return result
+ }else {
+    return target
+ }
+}
+
+function deepClone4(target,map=new Map()){
+  if(typeof target === 'object' && target !== null){
+    // 克隆数据之前，进行判断，数据之前是否克隆过  
+    let cache = map.get(target)
+    if(cache){
+    return cache
+    }
+    let isArray = Array.isArray(target)
+    const result = isArray ? [] : {}
+    // 将新的结果存入到容器中
+    map.set(target, result)
+    if(isArray){
+      target.forEach((item,index) => {
+        result[index] = deepClone4(item,map)
+      })
+    }else {
+      Object.keys(target).forEach(key => {
+        result[key] = deepClone4(target[key],map)
+      })
+    }
+    return result
+  }else {
+     return target
+  }
+ }
